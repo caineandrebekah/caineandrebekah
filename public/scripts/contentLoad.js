abad51ee3d0a3f927cb047558bf9e2e2455c238c
Caine J Lloyd <caine.lloyd.nielsen@gmail.com>
2018-11-10 17:28:06 -0700

    Functions testing, navigation fix, index intro, image overlay options

--- public/scripts/contentLoad.js
+++ public/scripts/contentLoad.js
@@ -2,13 +2,25 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
+      document.getElementById("demoDoc").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "/content/content.txt", true);
   xhttp.send();
 }
 
+function loadRoute() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demoRoute").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://us-central1-caineandrebekah.cloudfunctions.net/router", true);
+  xhttp.send();
+}
+
 $(function(){
   $("#footer").load("/templates/footer-snipit.html"); 
 });
+
