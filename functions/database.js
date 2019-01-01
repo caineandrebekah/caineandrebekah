031d9c0939f048e916c7489cc182d602c5ca6e94
Caine J Nielsen <caine.lloyd.nielsen@gmail.com>
2019-01-01 01:41:22 -0700

    fixing firebase token issues

--- functions/database.js
+++ functions/database.js
@@ -1,6 +1,6 @@
 /////////////// Firebase Realtime Database and Cloud Firestore
 const admin = require("firebase-admin");
-const serviceAccount = require("./caineandrebekah-firebase-adminsdk-k6r6u-6ff7566357.json")
+const serviceAccount = require("./fb-admin/caineandrebekah-firebase-adminsdk-k6r6u-6ff7566357.json")
 
 const firebaseConfig = {
     credential: admin.credential.cert(serviceAccount),
